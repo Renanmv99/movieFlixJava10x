5fd3f1fd89382319dd5478be9b001a0bebb739c4
Renan Vicente <renanvicente@gmail.com>
2025-09-02 21:18:46 -0300

    feat: adiciona mais endpoints pro movie

--- src/main/java/com/movieflix/controller/MovieController.java
+++ src/main/java/com/movieflix/controller/MovieController.java
@@ -1,13 +1,39 @@
 package com.movieflix.controller;
 
+import com.movieflix.controller.request.MovieRequest;
+import com.movieflix.controller.response.MovieResponse;
+import com.movieflix.entity.Movie;
+import com.movieflix.mapper.MovieMapper;
 import com.movieflix.service.MovieService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movieflix/movie")
 @RequiredArgsConstructor
 public class MovieController {
     private final MovieService movieService;
+
+    @PostMapping
+    public ResponseEntity<MovieResponse> save(@RequestBody MovieRequest movie) {
+        Movie savedMovie = movieService.save(MovieMapper.toMovie(movie));
+        return ResponseEntity.ok(MovieMapper.toMovieResponse(savedMovie));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<MovieResponse>> findAll() {
+        return ResponseEntity.ok(movieService.findAll().stream()
+                .map(MovieMapper::toMovieResponse)
+                .toList());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MovieResponse> findById(@PathVariable Long id) {
+        return movieService.findById(id)
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
